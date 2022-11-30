47fd28eafe65d04ebd48e041cf163e7c9c1d16a9
Tim Moriarity <moriarity.t@northeastern.edu>
2022-11-30 13:13:16 -0500

    Update commit

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 The "process.py" and "readit_all.py" files each hold a function used in the "penguins.py" script.
 
 The readit_all function is used to read and parse the csv.
+
 The process function is used to remove any missing data.
+
 #### It is import that both of these files are saved in the same directory as the penguins file.
 
 
--- penguins.py
+++ penguins.py
@@ -1,22 +1,27 @@
-# Import libraries and files:
+# Import libraries and files
 
 import pandas as pd
 import readit_all
 import process
 import matplotlib as plt
 
-# Import the penguins dataset:
+#Import the penguins dataset
 
-penguins = pd.read_csv("https://raw.githubusercontent.com/allisonhorst/palmerpenguins/master/inst/extdata/penguins.csv")
+curl -O https://raw.githubusercontent.com/allisonhorst/palmerpenguins/master/inst/extdata/penguins.csv
 
 readit_all(penguins)
 
-process(data)
+X, Y, Z = process(data)
 
 for s in species:
-    X_subset = [d for i, d in enumerate(X) if s == Z[i]]
-    Y_subset = [d for i, d in enumerate(Y) if s == Z[i]]
+  X_subset=[x_sub for x_sub,z_sub in zip(X,Z) if z_sub==s]
+  Y_subset=[y_sub for y_sub,z_sub in zip(Y,Z) if z_sub==s]
 
-    plt.scatter(X_subset, Y_subset, label=s)
+  # another way that does the same thing as 2 lines above
+  X_subset = [d for i, d in enumerate(X) if s == Z[i]]
+  Y_subset = [d for i, d in enumerate(Y) if s == Z[i]]
+
+  plt.scatter(X_subset, Y_subset, label=s)
+
 plt.legend()
 plt.show()
